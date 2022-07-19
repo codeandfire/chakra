f017bc59909efc99baa9be5dc19ff122329d2722
Saniya Maheshwari <saniya.mah@gmail.com>
2022-07-19 09:36:11 +0530

    `Command` class with subcommand, arguments, flags and env. variables.
    Tests are appropriately changed/added/removed.

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 import unittest
 from pathlib import Path
@@ -11,29 +12,108 @@ from tempfile_patch import tempfile
 
 class TestCommand(unittest.TestCase):
 
+    @unittest.skipIf(os.name == 'nt', 'on windows system')
     def test_echo(self):
         """Run an `echo` command."""
 
-        result = Command('echo foo').run()
+        result = Command('echo', positional_args=['foo']).run()
         assert result.returncode == 0
         assert result.stdout.strip() == 'foo'
         assert result.stderr.strip() == ''
 
-    def test_error(self):
-        """Run a command which gives an error."""
+    @unittest.skipIf(os.name == 'nt', 'on windows system')
+    def test_ls(self):
+        """Run an `ls -lh` command."""
 
-        result = Command('foo').run()
-        assert result.returncode != 0
-        assert result.stdout.strip() == ''
-        assert result.stderr.strip() != ''
+        result = Command('ls', flags=['-l', '-h']).run()
 
-    def test_empty(self):
-        """Run a command which is the empty string ``."""
-
-        result = Command('').run()
         assert result.returncode == 0
-        assert result.stdout.strip() == ''
+
+        # output of `ls -lh` will always have multiple lines.
+        assert len(result.stdout.strip().split(os.linesep)) > 1
+
         assert result.stderr.strip() == ''
+
+    def test_mkdir(self):
+        """Run a `mkdir` command against an existing directory."""
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            os.chdir(temp_dir)
+            Path('foo').mkdir()
+
+            # `mkdir` against an existing directory will always throw an error.
+            with self.assertRaises(subprocess.CalledProcessError) as exc:
+                Command('mkdir', positional_args=['foo']).run()
+
+        exc = exc.exception
+        assert exc.returncode != 0
+        assert exc.stdout.strip() == ''
+        assert exc.stderr.strip() != ''
+
+    def test_python_c(self):
+        """Run a `python -c` command."""
+
+        result = Command('python', optional_args={'-c': "print('Hello, world!')"}).run()
+        assert result.returncode == 0
+        assert result.stdout.strip() == 'Hello, world!'
+        assert result.stderr.strip() == ''
+
+    def test_pip(self):
+        """Run the command `pip install foo bar baz --find-links file:///foo/bar --progress-bar off --isolated --no-color`."""
+
+        with self.assertRaises(subprocess.CalledProcessError) as exc:
+            command = Command(
+                'pip',
+                subcommand='install',
+                positional_args=['foo', 'bar', 'baz'],
+                optional_args={
+                    '--find-links': Path('/foo/bar').as_uri(),
+                    '--progress-bar': 'off',
+                },
+                flags=['--isolated', '--no-color'],
+            )
+            command.run()
+
+        exc = exc.exception
+        assert exc.returncode != 0
+        assert exc.stdout.strip().startswith('Looking in links:')
+        for line in exc.stderr.strip().split(os.linesep):
+            assert line.startswith('WARNING') or line.startswith('ERROR')
+
+    # NOTE: this test is currently failing. Shows how we are not able to support `python
+    # -m` commands yet.
+    @unittest.skip('currently not working')
+    def test_python_m(self):
+        """Run a `python -m` command."""
+
+        result = Command('python', optional_args={'-m': 'unittest discover -h'}).run()
+        assert result.returncode == 0
+        assert result.stdout.strip().startswith('usage: python -m unittest discover')
+        assert result.stderr.strip() == ''
+
+    def test_invalid(self):
+        """Run an invalid command, i.e. a command that does not exist."""
+
+        with self.assertRaises(FileNotFoundError):
+            Command('foo').run()
+
+    @unittest.skipIf(os.name == 'nt', 'on windows system')
+    def test_env_vars_sh(self):
+        """Verify that environment variables are accessible to the command."""
+
+        command = Command('/bin/sh', optional_args={'-c': 'echo $FOO $BAR'},
+                          env_vars={'FOO': 'bar', 'BAR': 'foo'})
+        result = command.run()
+        assert result.stdout.strip() == 'bar foo'
+
+    @unittest.skipUnless(os.name == 'nt', 'on non-windows system')
+    def test_env_vars_powershell(self):
+        """Verify that environment variables are accessible to the command."""
+
+        command = Command('pwsh', optional_args={'-Command': 'echo $env:Foo $env:Bar'},
+                          env_vars={'Foo': 'bar', 'Bar': 'foo'})
+        result = command.run()
+        assert result.stdout.strip() == 'bar foo'
 
 
 class TestDevDeps(unittest.TestCase):
@@ -104,8 +184,9 @@ class TestEnvironment(unittest.TestCase):
             # are being run can interfere with this test.
 
             env = Environment(Path('.venv'))
-            assert env.create_command == \
-                Command('virtualenv .venv --download --activators python')
+            assert env.create_command == Command('virtualenv', positional_args=['.venv'],
+                                                 optional_args={'--activators': 'python'},
+                                                 flags=['--download'])
 
     @unittest.expectedFailure
     def test_path_is_a_path(self):
