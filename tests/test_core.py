440e44a2d7e5a5350363bd510881d24c9fc979d3
Saniya Maheshwari <saniya.mah@gmail.com>
2022-08-01 13:45:40 +0530

    Restructuring.
     - Moved `Config` class to a separate `config` module.
     - Moved its tests to a separate `test_config` module.
     - Then `tests.py` contains tests only for classes in the `core`
       module and therefore is appropriately renamed to `test_core.py`.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,10 @@
 import os
 import subprocess
 import sys
-import textwrap
 import unittest
 from pathlib import Path
 
-from chakra.core import Command, Config, DevDeps, Environment, Hook
+from chakra.core import Command, DevDeps, Environment, Hook
 
 # load a patched version of `tempfile`.
 from tempfile_patch import tempfile
@@ -287,49 +286,3 @@ class TestEnvironment(unittest.TestCase):
 
             with self.assertRaises(RuntimeError):
                 Environment(env_path).create_command.run()
-
-
-class TestConfig(unittest.TestCase):
-
-    def test(self):
-        """Test a sample `pyproject.toml` configuration."""
-
-        with tempfile.TemporaryDirectory() as temp_dir:
-            config_file = Path(temp_dir) / Path('pyproject.toml')
-            
-            with open(config_file, 'w') as f:
-                f.write(textwrap.dedent("""
-                    [project]
-                    name = "foo"
-                    version = "0.1.0"
-                    
-                    [build-system]
-                    requires = ["bar", "baz"]
-                    
-                    [tool.setuptools.packages.find]
-                    where = ["src"]
-                    
-                    [tool.chakra]
-                    env = "env"
-                    
-                    [tool.chakra.dev-deps]
-                    docs = ["sphinx"]
-                    lint = ["mypy", "flake8"]
-
-                    [tool.chakra.source]
-                    packages = ["foo"]
-                    """
-                ))
-                
-            config = Config(config_file)
-
-        metadata_text = config.metadata.text()
-        assert len(metadata_text.strip().split('\n')) == 3
-
-        assert config.env.path == Path('env')
-        assert config.build_env.path == Path('.build-venv')
-
-        assert config.dev_deps['docs'] == ['sphinx']
-        assert config.dev_deps['lint'] == ['mypy', 'flake8']
-
-        assert config.build_deps['build'] == ['bar', 'baz']
