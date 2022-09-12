e127cacf4a60c3db064cea46eabf3543f8d54174
Saniya Maheshwari <saniya.mah@gmail.com>
2022-09-12 08:01:21 +0530

    Backed out changeset 8a543fa6083d

--- src/chakra/core.py
+++ src/chakra/core.py
@@ -1,5 +1,3 @@
-import enum
-import functools
 import os
 import pathlib
 import shlex
@@ -59,44 +57,6 @@ class Command(object):
         env_vars = self.env_vars.copy()
         env_vars['PATH'] = os.environ['PATH']    # pass the current PATH
         return _subprocess_run(self.tokens, capture_output=capture_output, env=env_vars)
-
-
-class OpSystem(enum.Enum):
-    WINDOWS = 'nt'
-    LINUX = 'linux'
-    MACOS = 'darwin'
-
-    @classmethod
-    @functools.cache
-    def find(cls):
-        # plat_os = platform OS, cand_os = candidate OS
-        if os.name == 'posix':
-            plat_os = Command(['uname', '-s']).run().stdout.lower()
-        else:
-            plat_os = os.name
-        for cand_os in cls:
-            if plat_os == cand_os.value:
-                return cand_os
-        raise NotSupportedError(f'unsupported operating system (or kernel) {plat_os!r}')
-
-
-class Arch(enum.Enum):
-    INTEL_AMD_32_BIT = ('x86', 'i386', 'i586', 'i686')
-    INTEL_AMD_64_BIT = ('x86_64', 'amd64')
-    ARM_64_BIT = ('arm64', 'aarch64')
-
-    @classmethod
-    @functools.cache
-    def find(cls, opsys):
-        # plat_ar = platform arch., cand_ar = candidate arch.
-        if opsys == OpSystem.WINDOWS:
-            plat_ar = os.environ['PROCESSOR_ARCHITECTURE'].lower()
-        else:
-            plat_ar = Command(['uname', '-m']).run().stdout
-        for cand_ar in cls:
-            if plat_ar in cand_ar.value:
-                return cand_ar
-        raise NotSupportedError(f'unsupported architecture {plat_ar!r}')
 
 
 class Hook(Command):
