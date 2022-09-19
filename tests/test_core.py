3b8d95b76144077e7103a86ad59a0bf6fe4df971
Saniya Maheshwari <saniya.mah@gmail.com>
2022-09-19 13:45:57 +0530

    Removed all docstrings from tests.
    Other minor modifications made (removed blank lines, added comments, etc.)

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,8 +15,6 @@ class TestCommand(unittest.TestCase):
 
     @unittest.skipIf(OpSystem.find() == OpSystem.WINDOWS, 'on windows system')
     def test_echo(self):
-        """Run an `echo` command."""
-
         result = Command(['echo', 'foo']).run()
         assert result.returncode == 0
         assert result.stdout == 'foo'
@@ -24,8 +22,6 @@ class TestCommand(unittest.TestCase):
 
     @unittest.skipIf(OpSystem.find() == OpSystem.WINDOWS, 'on windows system')
     def test_ls(self):
-        """Run an `ls -lh` command."""
-
         result = Command(['ls', '-l', '-h']).run()
         assert result.returncode == 0
         # output of `ls -lh` will always have multiple lines.
@@ -34,8 +30,6 @@ class TestCommand(unittest.TestCase):
 
     @unittest.skipIf(OpSystem.find() == OpSystem.WINDOWS, 'on windows system')
     def test_mkdir(self):
-        """Run a `mkdir` command against an existing directory."""
-
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
             pathlib.Path('foo').mkdir()
@@ -48,8 +42,6 @@ class TestCommand(unittest.TestCase):
         assert result.stderr != ''
 
     def test_python_c(self):
-        """Run a `python -c` command."""
-
         result = Command(
             ['python', '-c', "print('Hello, world!')"]).run()
         assert result.returncode == 0
@@ -57,8 +49,6 @@ class TestCommand(unittest.TestCase):
         assert result.stderr == ''
 
     def test_pip(self):
-        """Run the command `pip install foo bar baz --find-links file:///foo/bar`."""
-
         result = Command(
             ['pip', 'install', 'foo', 'bar', 'baz', '--find-links',
              pathlib.Path('/foo/bar').as_uri()],
@@ -69,8 +59,6 @@ class TestCommand(unittest.TestCase):
             assert line.startswith('WARNING') or line.startswith('ERROR')
 
     def test_python_m(self):
-        """Run a `python -m` command."""
-
         result = Command(
             ['python', '-m', 'unittest', 'discover', '-h']).run()
         assert result.returncode == 0
@@ -78,8 +66,6 @@ class TestCommand(unittest.TestCase):
         assert result.stderr == ''
 
     def test_invalid(self):
-        """Run an invalid command, i.e. a command that does not exist."""
-
         result = Command(['foo']).run()
         assert result.returncode != 0
         assert result.stdout == ''
@@ -87,8 +73,6 @@ class TestCommand(unittest.TestCase):
 
     @unittest.skipIf(OpSystem.find() == OpSystem.WINDOWS, 'on windows system')
     def test_env_vars_sh(self):
-        """Verify that environment variables are accessible to the command."""
-
         command = Command(['/bin/sh', '-c', 'echo $FOO $BAR'],
                           env_vars={'FOO': 'bar', 'BAR': 'foo'})
         result = command.run()
@@ -96,8 +80,6 @@ class TestCommand(unittest.TestCase):
 
     @unittest.skipUnless(OpSystem.find() == OpSystem.WINDOWS, 'on non-windows system')
     def test_env_vars_powershell(self):
-        """Verify that environment variables are accessible to the command."""
-
         command = Command(['powershell', '-Command', 'echo $env:Foo $env:Bar'],
                           env_vars={'Foo': 'bar', 'Bar': 'foo'})
         result = command.run()
@@ -107,8 +89,6 @@ class TestCommand(unittest.TestCase):
 class TestHook(unittest.TestCase):
 
     def test_python(self):
-        """Test a Python hook."""
-
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
             with open('foo.py', 'w') as f:
@@ -118,8 +98,6 @@ class TestHook(unittest.TestCase):
 
     @unittest.skipIf(OpSystem.find() == OpSystem.WINDOWS, 'on windows system')
     def test_bash(self):
-        """Test a Bash hook."""
-
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
             with open('foo', 'w') as f:
@@ -130,8 +108,6 @@ class TestHook(unittest.TestCase):
 
     @unittest.skipIf(OpSystem.find() == OpSystem.WINDOWS, 'on windows system')
     def test_bash_sh_extension(self):
-        """Test a Bash hook with an `.sh` extension."""
-
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
             with open('foo.sh', 'w') as f:
@@ -141,8 +117,6 @@ class TestHook(unittest.TestCase):
 
     @unittest.skipUnless(OpSystem.find() == OpSystem.WINDOWS, 'on non-windows system')
     def test_powershell(self):
-        """Test a Powershell hook."""
-
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
             with open('foo.ps1', 'w') as f:
@@ -151,8 +125,6 @@ class TestHook(unittest.TestCase):
         assert result.stdout == 'foo'
 
     def test_unsupported(self):
-        """Test a hook with an unsupported extension."""
-
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
             with open('foo.bat', 'w') as f:
@@ -164,8 +136,6 @@ class TestHook(unittest.TestCase):
 class TestEnvironment(unittest.TestCase):
 
     def test_create(self):
-        """Test creation of an environment."""
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             env = Environment(env_path)
@@ -177,33 +147,21 @@ class TestEnvironment(unittest.TestCase):
             assert env.pyvenv_cfg.exists()
 
     def test_activate(self):
-        """Test if activation of an environment works.
-
-        This test works by checking the value of `PATH` before and after activating the
-        environment. After activating the environment, `PATH` should contain some
-        directories that are relative to the directory of the environment.
-        """
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             virtualenv.cli_run([str(env_path)])
             env = Environment(env_path)
             env.create()
             assert not env.is_activated
+            # no path in `sys.path` should have anything to do with `env_path`
             assert all([not pathlib.Path(path).is_relative_to(env_path) for path in sys.path])
 
             env.activate()
             assert env.is_activated
+            # at least one path in `sys.path` should be relative to `env_path`
             assert any([pathlib.Path(path).is_relative_to(env_path) for path in sys.path])
 
     def test_setuptools_wheel_not_installed(self):
-        """Test that `setuptools` and `wheel` are not installed in the environment.
-
-        The packages `setuptools` and `wheel` are not required by Chakra; if required by
-        other packages as build-time dependencies, they can be installed at that time. Not
-        including these two packages may save some time while creating the environment.
-        """
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             env = Environment(env_path)
@@ -212,8 +170,6 @@ class TestEnvironment(unittest.TestCase):
             assert not env.has_installed('wheel')
 
     def test_pip_installed(self):
-        """Test that `pip` is installed in the environment."""
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             env = Environment(env_path)
@@ -221,8 +177,6 @@ class TestEnvironment(unittest.TestCase):
             assert env.has_installed('pip')
 
     def test_pip_latest_version(self):
-        """The environment should come with the latest version of Pip."""
-
         version_cmd = Command(['pip', '--version'])
         upgrade_cmd = Command(['pip', 'install', '--upgrade', 'pip'])
 
@@ -238,8 +192,6 @@ class TestEnvironment(unittest.TestCase):
         assert version == new_version
 
     def test_package_installs(self):
-        """Test that package installs work in the environment."""
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             env = Environment(env_path)
@@ -253,8 +205,6 @@ class TestEnvironment(unittest.TestCase):
             assert env.has_installed('build', '0.4.0')
 
     def test_package_imports(self):
-        """Test that packages can be imported within the environment."""
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             env = Environment(env_path)
@@ -268,8 +218,6 @@ class TestEnvironment(unittest.TestCase):
         assert result.stderr == ''
 
     def test_console_scripts(self):
-        """Test that console scripts work in the environment."""
-
         with tempfile.TemporaryDirectory() as tmp:
             env_path = pathlib.Path(tmp) / '.venv'
             env = Environment(env_path)
--- tests/test_utils_rfc822.py
+++ tests/test_utils_rfc822.py
@@ -3,7 +3,6 @@ import textwrap
 
 from chakra.utils import rfc822
 
-
 class Test(unittest.TestCase):
 
     # sample data.
@@ -14,7 +13,6 @@ class Test(unittest.TestCase):
         'baz': ['foo'],
     }
     _body = 'foo bar\nbaz'
-
     _text = textwrap.dedent("""
         foo: bar
         foo: baz
@@ -31,51 +29,34 @@ class Test(unittest.TestCase):
         baz
     """).strip()
 
-
     def test_dumps(self):
-        """Test conversion to RFC 822 formatted text."""
-
         assert rfc822.dumps(self._headers, self._body) == self._text
 
     def test_loads(self):
-        """Test conversion from RFC 822 formatted text back to data."""
-
         assert rfc822.loads(self._text) == (self._headers, self._body)
 
     def test_identity(self):
-        """Test whether identity holds.
-
-        Essentially, a piece of data converted to RFC 822 formatted text and back
-        to data should be exactly the same as the original data, and similarly for text
-        converted to data and back to text.
-        """
-
+        # data converted to text and back should be the same and likewise for text
         assert rfc822.loads(rfc822.dumps(self._headers, self._body)) == \
             (self._headers, self._body)
         assert rfc822.dumps(*rfc822.loads(self._text)) == self._text
 
-
 class TestBoundaryCases(unittest.TestCase):
 
     def test_empty(self):
-        """No data."""
-
         headers, body = {}, ''
         text = '\n'
         assert rfc822.dumps(headers, body) == text
         assert rfc822.loads(text) == (headers, body)
 
     def test_empty_body(self):
-        """Headers but no body."""
-
         headers = {
             'foo': ['bar', 'baz', 'foo\nbar'],
             'baz': ['foo'],
         }
         body = ''
-
         # the `\n\n` is added outside the `textwrap.dedent()` argument so that it is not
-        # removed by the `.strip()` method.
+        # removed by the `.strip()` method
         text = textwrap.dedent("""
             foo: bar
             foo: baz
@@ -87,8 +68,6 @@ class TestBoundaryCases(unittest.TestCase):
         assert rfc822.loads(text) == (headers, body)
 
     def test_empty_headers(self):
-        """Body but no headers."""
-
         headers = {}
         body = 'foo bar\nbaz'
         text = '\nfoo bar\nbaz'
@@ -97,8 +76,6 @@ class TestBoundaryCases(unittest.TestCase):
 
     @unittest.skip('currently failing')
     def test_empty_values_case1(self):
-        """Empty lists as values."""
-
         headers = {'foo': [], 'bar': []}
         body = 'foo bar\nbaz'
         text = textwrap.dedent("""
@@ -112,8 +89,6 @@ class TestBoundaryCases(unittest.TestCase):
 
     @unittest.skip('currently failing')
     def test_empty_values_case2(self):
-        """Empty strings as individual values."""
-
         headers = {
             'foo': ['bar', '', ''],
             'bar': ['', 'baz'],
@@ -132,11 +107,8 @@ class TestBoundaryCases(unittest.TestCase):
         assert rfc822.dumps(headers, body) == text
 
     def test_two_newlines_in_value_case1(self):
-        """A value in the headers contains two (or more) consecutive newlines.
-
-        The expected behaviour is that any consecutive newlines in the headers should be
-        collapsed down to a single newline.
-        """
+        # value in the headers contains two or more consecutive newlines, it should be
+        # collapsed down to a single newline.
 
         headers = {
             'foo': ['bar', 'baz', 'foo\n\nbar'],
@@ -160,12 +132,6 @@ class TestBoundaryCases(unittest.TestCase):
         assert rfc822.dumps(headers, body) == text
 
     def test_two_newlines_in_value_case2(self):
-        """Two (or more) consecutive newlines in headers must cause parsing issues.
-
-        This is somewhat a different test because it verifies that our implementation HAS
-        (instead of does NOT have) a particular flaw.
-        """
-
         text = textwrap.dedent("""
             foo: bar
             foo: baz
@@ -183,22 +149,15 @@ class TestBoundaryCases(unittest.TestCase):
             foo bar
             baz
         """).strip()
-
-        # the headers and body from which the above text could have possibly been
-        # generated.
+        # note that this data does not uniquely produce the above text
         headers = {
             'foo': ['bar', 'baz', 'foo\n\nbar'],
             'bar': ['baz\nfoo\n\n\nbar', 'foo', 'baz'],
         }
         body = 'foo bar\nbaz'
-        assert rfc822.loads(text) != (headers, body)
+        assert rfc822.loads(text) != (headers, body)   # parsing fails
 
     def test_colon_without_space(self):
-        """Using a colon without a space following it must cause parsing issues.
-
-        Again, this verifies that our implementation HAS a particular flaw.
-        """
-
         text = textwrap.dedent("""
             foo:bar
             foo:baz
@@ -208,9 +167,7 @@ class TestBoundaryCases(unittest.TestCase):
             foo bar
             baz
         """).strip()
-
-        # the headers and body from which the above text could have possibly been
-        # generated.
+        # note that this data does not uniquely produce the above text
         headers = {'foo': ['bar', 'baz'], 'bar': ['baz', 'foo']}
         body = 'foo bar\nbaz'
-        assert rfc822.loads(text) != (headers, body)
+        assert rfc822.loads(text) != (headers, body)   # parsing fails
