61fee3dc9977549117ab43987050583564acff18
Saniya Maheshwari <saniya.mah@gmail.com>
2022-09-19 10:37:24 +0530

    Fix: Comment in `utils/__init__.py`.

--- src/chakra/utils/__init__.py
+++ src/chakra/utils/__init__.py
@@ -1,7 +1,7 @@
 # import the monkey-patched modules.
-
 from .tempfile_patch import tempfile
 from .tomllib_patch import tomllib
+
 from .version import Version
 
 class NotSupportedError(Exception):
