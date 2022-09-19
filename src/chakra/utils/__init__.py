cc4abe546a23989f8ce9d1d03af414385ef0f516
Saniya Maheshwari <saniya.mah@gmail.com>
2022-09-19 10:36:17 +0530

    Moved `NotSupportedError` to separate `errors` module.

--- src/chakra/utils/__init__.py
+++ src/chakra/utils/__init__.py
@@ -4,6 +4,3 @@ from .dirtree import HDirectory, HFile
 from .tempfile_patch import tempfile
 from .tomllib_patch import tomllib
 from .version import Version
-
-class NotSupportedError(Exception):
-    pass
