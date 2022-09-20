4117690f4826f794585eb3e642658d01dae68922
Saniya Maheshwari <saniya.mah@gmail.com>
2022-09-20 12:21:07 +0530

    Minor changes to `utils` subpackage.
    Removed blank lines, modified comments etc.

--- src/chakra/utils/ini.py
+++ src/chakra/utils/ini.py
@@ -1,46 +1,29 @@
-"""Simple implementation of the INI configuration format.
-
-This implementation is simply a thin wrapper around the standard library `configparser`
-module.
-"""
-
-
 from configparser import ConfigParser
 import io
 
-
 __all__ = ['dumps', 'dump', 'loads', 'load']
 
+# this is a simple wrapper around the `ConfigParser` module
 
 def dumps(data):
     parser = ConfigParser(delimiters=('=',))
     parser.read_dict(data)
-
-    # `ConfigParser` does not provide a method to write the INI formatted text to a
-    # string, i.e. it can only be written to a file.
-    # hence, we use an `io.StringIO` object as a workaround.
-
+    # `io.StringIO`: a workaround for `ConfigParser` not having a method to write to a string
     with io.StringIO() as s:
         parser.write(s, space_around_delimiters=True)
         text = s.getvalue().strip()
-
     return text
-
 
 def dump(data, fp):
     return fp.write(dumps(data))
 
-
 def loads(text):
     parser = ConfigParser(delimiters=('=',))
     parser.read_string(text)
-
     data = {}
     for section in parser.sections():
         data[section] = dict(parser.items(section))
-
     return data
-
 
 def load(fp):
     return loads(fp.read().strip())
